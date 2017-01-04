29ae344df60eb377692b63c5c9ee3fc7b29ebc39
kris21345 <kris21345@gmail.com>
2017-01-04 18:01:23 +0100

    Just minor things.

--- AdbOverWifi/src/AdbOverWifi/Program.cs
+++ AdbOverWifi/src/AdbOverWifi/Program.cs
@@ -10,32 +10,33 @@ namespace netcore_redis
 {
     public class Program
     {
-		//how to run:
-		//dotnet   run   IP:PORT   PATH_TO_APK  TIMEOUT
-		
-		//dotnet run 192.168.4.101:5556 /home/koma/koma/apk/z.apk 5
+        //how to run:
+        //dotnet   run   IP:PORT   PATH_TO_APK  TIMEOUT
+        
+        //dotnet run 192.168.4.101:5556 /home/koma/koma/apk/z.apk 5
         public static void Main(string[] args)
         {	
-			int timeout = int.Parse(args[2]);
-			runADB("logcat -c", 1);	//this is done instantly
+            int timeout = int.Parse(args[2]);
+            runADB("logcat -c", 1);	//this is done instantly
             runADB("connect " + args[0],1);
             //Thread threadInstall = new Thread(() => runADB("install " + args[1], 10));
-			//threadInstall.Start();
-			Console.WriteLine("\n************LOGCAT STARTED****************\n");
-			runADB("logcat", timeout);
-			//runADB("logcat ActivityManager:I *:S", timeout);
-			Console.WriteLine("END MAIN.");
+            //threadInstall.Start();
+            Console.WriteLine("\n************LOGCAT STARTED****************\n");
+            runADB("logcat", timeout);
+            Console.WriteLine("\n************LOGCAT ENDED*****************\n");
+            //runADB("logcat ActivityManager:I *:S", timeout);
+            Console.WriteLine("END MAIN.");
             Console.ReadLine();
         }
 
         private static void runADB(String arguments, int t)
         {
-			int timeout = t;
-			DateTime now;
-			DateTime startTime;
-			TimeSpan difference ;
+            int timeout = t;
+            DateTime now;
+            DateTime startTime;
+            TimeSpan difference;
 
-			//Console.WriteLine("Start time is: " + startTime);
+            //Console.WriteLine("Start time is: " + startTime);
 
             String line="";
             Process proc = new Process
@@ -50,23 +51,23 @@ namespace netcore_redis
                 }
             };
             proc.Start();
-			//int i=0;
-			bool waitLogcat=true;
-			
-			startTime = DateTime.UtcNow;
+            //int i=0;
+            bool waitLogcat=true;
+            
+            startTime = DateTime.UtcNow;
             while (waitLogcat)
             {
                 line = proc.StandardOutput.ReadLine();
-			
-				now = DateTime.UtcNow;
-				difference = now.Subtract(startTime);
-				if (difference.TotalSeconds > timeout)
-					waitLogcat=false;
+            
+                now = DateTime.UtcNow;
+                difference = now.Subtract(startTime);
+                if (difference.TotalSeconds > timeout)
+                    waitLogcat=false;
                 if(!String.IsNullOrWhiteSpace(line))//treba da se testira
-				    Console.WriteLine(line);
+                    Console.WriteLine(line);
             };
-			
-			Console.WriteLine("END runADB");
+            
+            Console.WriteLine("END runADB");
         }
     } 
 }
