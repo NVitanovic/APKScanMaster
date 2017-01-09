90c564efa057069366774583ae50878111b0ff3b
Ivan Koma <ivankoma@outlook.com>
2017-01-09 20:47:47 +0200

    added tests for usage quota (6000 per hours)

--- RedisTest/src/RedisTest/Program.cs
+++ RedisTest/src/RedisTest/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 using System.Net.Http;
+using System.Threading;
 
 namespace RedisTest
 {
@@ -11,7 +12,42 @@ namespace RedisTest
     {
         public static void Main(string[] args)
         {
-            /*ConnectionMultiplexer redis1 = ConnectionMultiplexer.Connect("192.168.4.201:7000,192.168.4.211:7000");
+            /*
+            //komin test redis-a
+            ConnectionMultiplexer redis1 = ConnectionMultiplexer.Connect("localhost");
+            //ConnectionMultiplexer redis1 = ConnectionMultiplexer.Connect("192.168.4.201:7000");
+            IDatabase db1 = redis1.GetDatabase();
+            int max = 1024;
+            int key, value;
+            
+            for(key = 0; key < max; key++)
+            {
+                value = key / 2;
+                Console.Write("Writing to Redis" + key + ":" + value);
+                
+                Console.WriteLine(" - result is " + db1.StringSet(key.ToString(), value));
+            }
+            
+            
+            
+            for (key = 0; key < max; key++)
+            {
+                Console.WriteLine("Reading key {0}, value is {1}", key, db1.StringGet(key.ToString()));
+            }
+            Thread.Sleep(5000);
+            Random rnd = new Random();
+            while (true)
+            {
+                for (key = 0; key < max; key++)
+                {
+                    int randomKey = rnd.Next(1, 6000);
+                    Console.WriteLine("Random key {0}, value is {1}", randomKey, db1.StringGet(randomKey.ToString()));
+                }
+                Thread.Sleep(5000);
+            }
+            */
+
+            ConnectionMultiplexer redis1 = ConnectionMultiplexer.Connect("192.168.4.201:7000,192.168.4.211:7000");
             IDatabase db1 = redis1.GetDatabase();
             String value1 = "redis1";
             db1.StringSet("rediskey1", value1);
@@ -27,7 +63,9 @@ namespace RedisTest
             IDatabase db3 = redis1.GetDatabase();
             String value3 = "redis3";
             db3.StringSet("rediskey1", value3);
-            Console.WriteLine(db1.StringGet("rediskey1"));*/
+            Console.WriteLine(db1.StringGet("rediskey1"));
+
+            /*
             HttpClient client = new HttpClient();
             var getresponse = client.GetAsync("http://httpbin.org/get");
             Console.WriteLine(getresponse.Result.Content.ReadAsStringAsync().Result.ToString());
@@ -43,6 +81,7 @@ namespace RedisTest
             var postresponse = client.PostAsync("http://httpbin.org/post", content);
 
             Console.WriteLine(postresponse.Result.Content.ReadAsStringAsync().Result.ToString());
+            */
             Console.ReadLine();
         }
 
